a3f6ccbd5f1ba9f883dc5c9e67df59c2d64c32c5
resister-boy <hassanpumped17@gmail.com>
2024-07-11 13:27:56 +0900

    feat: applicate all

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { PostModule } from '@/post/post.module';
@@ -9,34 +9,63 @@ import { UserController } from '@/user/user.controller';
 import { UserModule } from '@/user/user.module';
 import { TagController } from '@/tag/tag.controller';
 import { TagModule } from '@/tag/tag.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import swaggerConfig from '@/config/swagger.config';
-import { MongooseModule } from '@nestjs/mongoose';
+import mongodbConfig from '@/config/db.config';
+import { AuthMechanism, AuthSource, MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
+import { FirebaseModule } from '@/firebase/firebase.module';
+import { CrawlerModule } from '@/crawler/crawler.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from '@/common/filters/http-exception.filter';
+import { LoggerMiddleware } from '@/middleware/logger.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.STAGE}`,
-      load: [swaggerConfig],
+      load: [mongodbConfig, swaggerConfig],
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URL, {
-      user: process.env.MONGODB_USERNAME,
-      pass: process.env.MONGODB_PASSWORD,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('db.uri'),
+        dbName: configService.get<string>('db.dbName'),
+        user: configService.get<string>('db.username'),
+        pass: configService.get<string>('db.password'),
+        authMechanism: configService.get<AuthMechanism>('db.authMechanism'),
+        authSource: configService.get<AuthSource>('db.authSource'),
+      }),
+      inject: [ConfigService],
     }),
+    ScheduleModule.forRoot(),
     PostModule,
     ContentModule,
     CommentModule,
-    UserModule,
+    // UserModule,
     TagModule,
+    FirebaseModule,
+    CrawlerModule,
   ],
   controllers: [
     AppController,
     CommentController,
-    UserController,
+    // UserController,
     TagController,
   ],
-  providers: [AppService],
+  providers: [
+    AppService,
+    Logger,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    }
+  ],
 })
 
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*')
+  }
+}
