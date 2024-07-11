a0840162dd1f432f156d057c4698adca487e3560
resister-boy <hassanpumped17@gmail.com>
2024-07-11 13:21:12 +0900

    types: add auth type to export

---
+++ @types/auth.d.ts
@@ -0,0 +1,6 @@
+import { AuthMechanism, AuthSource } from "@nestjs/mongoose";
+
+export declare module '@nestjs/mongoose' {
+  export type AuthMechanism = AuthMechanism;
+  export type AuthSource = AuthSource
+}
