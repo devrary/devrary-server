bea08160bdf572526f1e70984fb51b375bf50a42
resister-boy <hassanpumped17@gmail.com>
2024-07-17 15:48:47 +0900

    rafactor: add book & code content

--- src/content/content.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class ContentRequestDto {
-  @IsNotEmpty()
-  @IsString()
-  contentId: string;
-}
--- src/content/content.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ContentService } from '@/content/content.service';
-import { ContentController } from '@/content/content.controller';
-
-@Module({
-  controllers: [ContentController],
-  providers: [ContentService],
-})
-export class ContentModule {}
